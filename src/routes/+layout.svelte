c1064fe3b93cf7c250e1d204bbc736ed89a1172f
nitromelon <112331897+nitromelon@users.noreply.github.com>
2025-06-28 21:21:58 +0700

    updated

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import "the-new-css-reset/css/reset.css";
+    import "$lib/styles/layout.css";
+	const { children } = $props();
+</script>
+
+{@render children()}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
 import devtoolsJson from 'vite-plugin-devtools-json';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import autoprefixer from "autoprefixer";
+import nested from "postcss-nested";
+import cssnano from "cssnano";
 
 export default defineConfig({
-	plugins: [sveltekit(), devtoolsJson()]
+	plugins: [sveltekit(), devtoolsJson()],
+	css: {
+		postcss: {
+			plugins: [
+				nested({}),
+				autoprefixer({}),
+				cssnano({
+					preset: "advanced",
+				}),
+			],
+		},
+	},
 });
